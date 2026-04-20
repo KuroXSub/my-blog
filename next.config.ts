804f346253f417542620e8be1c1aeab90e44b72b
KuroXSub <qurrotaayunliulinuha@gmail.com>
2026-04-20 20:14:24 +0700

    Update untuk cdn.sanity.io (image)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
+
   async headers() {
     return [
       {
